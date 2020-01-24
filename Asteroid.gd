ef0c2bfc9ebfc4374d125f4f598e8e9f3ed14ccf
Cesar <cesargiulietti@gmail.com>
2020-01-24 15:11:45 -0300

    added some logic to spawn big and small asteroids

--- Asteroid.gd
+++ Asteroid.gd
@@ -1,11 +1,16 @@
 extends KinematicBody2D
 
-export var bounce = 1.1
-
 var velocity = Vector2()
 var rotation_speed
 var screen_size
 var extents
+
+var textures = {
+	'big_asteroid':'res://meteorite_1.png',
+	'small_asteroid':'res://Meteorite_3.png'
+}
+
+onready var puff = get_node("puff")
 
 func _ready():
 	randomize()
@@ -13,16 +18,40 @@ func _ready():
 	velocity = Vector2(rand_range(30, 100), 0).rotated(rand_range(0, 2*PI))
 	rotation_speed = rand_range(-1.5, 1.5)
 	screen_size = get_viewport_rect().size
-	extents = get_node("Sprite").texture.get_size() / 2
+	#extents = get_node("Sprite").texture.get_size() / 2
+	init('big_asteroid', screen_size / 2)
 
+func init(ast_size, spawn_pos):
+	var scale = Vector2()
+	var texture = load(textures[ast_size])
+	
+	# according to the asteroid that we pass, we scale it different
+	# e.g if we pass as small asteroid we make it smaller.
+	if ast_size == 'small_asteroid':
+		scale.x = 0.25
+		scale.y = 0.25
+		get_node("CollisionShape2D").set_scale(Vector2(0.5,0.5))
+	else:
+		scale.x = 0.5
+		scale.y = 0.5
+	
+	# we set the texture and the scale
+	get_node("Sprite").set_texture(texture)
+	get_node("Sprite").set_scale(scale)
+	
+	extents = texture.get_size() / 2
+	set_position(spawn_pos)
+	
 func _physics_process(delta):
 	set_rotation(get_rotation() + rotation_speed * delta)
 	var ret = move_and_collide(velocity * delta)
 	
 	if ret:
-		#velocity += ret.normal * (ret.collider_velocity.length() * bounce)
+		var collision_point = ret.position
+		puff.set_global_position(collision_point)
+		puff.set_emitting(true)
 		velocity = velocity.bounce(ret.normal)
-	
+		
 	var position = get_position()
 	
 	if position.x > screen_size.x + extents.x:
